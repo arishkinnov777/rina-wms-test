dfe11e12a016a0e42a2482acfd06871efcc3a185
arishkinnov777 <arina-novikova2009@yandex.ru>
2022-06-10 08:09:40 +0300

    Add tests

--- src/main/java/TestWms.java
+++ src/main/java/TestWms.java
@@ -1,26 +1,34 @@
-import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class TestWms {
-    public void Test() {
+
+    public void modalTest() {
 
         step("Модалка создания/редактирования маршрутов", () -> {
         });
-
+    }
+    public void searchByCityTest() {
         step("Поиск по городам,ПВЗ", () -> {
         });
+    }
 
+    public void selectCheckboxesOnLeftSide() {
         step("Выбор чек-боксов с левой стороны", () -> {
         });
+    }
 
+    public void listPvzTest() {
         step("Списки ПВЗ городов (правая таблица)", () -> {
         });
+    }
 
+    public void numbersRouteTest() {
         step("Номера маршрутов", () -> {
         });
+    }
 
+    public void packingWallTest() {
         step("Стены упаковки", () -> {
         });
-
     }
 }
